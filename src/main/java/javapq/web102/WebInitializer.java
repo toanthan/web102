ec6398597fb8308629d9ab89914edaacf737d05c
Dat Tran <dat.tran@sentifi.com>
2019-08-11 15:34:08 +0700

    Fix mvc loader

--- src/main/java/javapq/web102/WebInitializer.java
+++ src/main/java/javapq/web102/WebInitializer.java
@@ -1,28 +1,25 @@
 package javapq.web102;
 
+import javapq.web102.config.SpringConfig;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 public class WebInitializer implements WebApplicationInitializer {
     @Override
-    public void onStartup(final ServletContext sc) throws ServletException {
+    public void onStartup(final ServletContext sc) {
 
         AnnotationConfigWebApplicationContext root =
                 new AnnotationConfigWebApplicationContext();
-
-        root.scan("javapq.web102");
-        sc.addListener(new ContextLoaderListener(root));
+        root.register(SpringConfig.class);
+        root.refresh();
 
         ServletRegistration.Dynamic appServlet =
-                sc.addServlet("mvc", new DispatcherServlet(new GenericWebApplicationContext()));
+                sc.addServlet("mvc", new DispatcherServlet(root));
         appServlet.setLoadOnStartup(1);
-        appServlet.addMapping("/");
+        appServlet.addMapping("/mvc/*");
     }
 }
